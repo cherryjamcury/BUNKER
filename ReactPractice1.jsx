7c215c2308b6e5cf3bc24029ac8b5e98d2568982
Alex Vlad <alexvlad@Alexs-MacBook-Air.local>
2025-06-22 14:37:03 +0900

    Some Drafts

--- ReactPractice1.jsx
+++ ReactPractice1.jsx
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+
+
+async function ConnectToAPI(URL, KEY = null){
+
+  const headers = {}
+
+  if(KEY){
+    headers['Authorization'] = KEY
+  }
+
+  try{
+    const response  = await fetch(
+      URL, {
+        method: 'GET',
+        headers: headers
+      })
+
+      const data = await response.json()
+      console.log(data)
+      return data 
+  }
+
+  catch(error){
+    console.error(error)
+    return null
+  }
+}
+
+
+
+function  HandleData(data){
+  if(Array.isArray(data)){
+    data.forEach(item => HandleData(item))
+  }
+  else if(data !== null && typeof data === 'object'){
+    Object.entries(data).forEach(([key , value])=>{
+      HandleData(value)
+    })
+  }
+  else {
+    console.log(data)
+  }
+
+}
+
+
 
 export default function ReactPractice1() {
+const URl = 'https://restcountries.com/v3.1/name/aruba?fullText=true'
+
+  useEffect(()=>{
+
+ConnectToAPI(URl).then(data => {
+  if(data){
+    HandleData(data)
+
+  }
+})
+
+  },[])
+
   return (
-    <div>ReactPractice1</div>
+    <div>Practicing connecting to an API, loading data, and using it in a project </div>
+
+    
   )
 }
